6e9acdf379301efc3b9ff6b02ef89998fa80d563
bikas.katwal <bikas.katwal10@gmail.com>
2021-10-21 21:26:35 +0530

    readme update

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 import os
 
 ROOT_SRC_DIR = os.path.dirname(os.path.abspath(__file__))
-USERNAME = os.environ.get('UNIVERSAL_SEARCH_QUS_USERNAME', 'admin')
-PASSWORD = os.environ.get('UNIVERSAL_SEARCH_QUS_PASSWORD', 'admin123')
+USERNAME = os.environ.get('APP_USERNAME', 'admin')
+PASSWORD = os.environ.get('APP_PASSWORD', 'admin123')
